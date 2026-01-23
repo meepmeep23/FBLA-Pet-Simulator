606b303d935aaf2a34a08971a73fd9379e940ff2
meepmeep23 <164576365+meepmeep23@users.noreply.github.com>
2026-01-23 15:36:03 -0700

    bye bye scripts

--- Scripts/duckRanchFenceCheck.gd
+++
@@ -1,7 +0,0 @@
-extends RayCast3D
-
-
-#Script for the raycasts on the ducks!
-func _process(_delta: float) -> void:
-	if is_colliding():
-		self.get_parent().get_parent().state = "think"
--- Scripts/return.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-
-func _on_pressed() -> void:
-	self.get_parent().get_parent().current_tab = 0
