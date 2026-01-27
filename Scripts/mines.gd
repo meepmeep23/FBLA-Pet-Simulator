ca494c43364297dc070f24537cd971c913ff45ee
meepmeep23 <164576365+meepmeep23@users.noreply.github.com>
2026-01-27 14:30:12 -0700

    Updated the comments on the Mines

--- Scripts/mines.gd
+++ Scripts/mines.gd
@@ -4,6 +4,7 @@ var rng = RandomNumberGenerator.new()
 
 @export var miningTime: float
 
+#List of the odds of the minerals
 var MineralList = {
 	Gold = 0.75,
 	Iron = 3,
@@ -105,11 +106,13 @@ func miniGameBehavior(delta):
 		self.get_parent().find_child("OmniLight3D").position.y = move_toward(get_parent().find_child("OmniLight3D").position.y, 15, delta * 4)
 	
 func _on_duck_list_item_clicked(index: int, _at_position: Vector2, _mouse_button_index: int) -> void:
+	#Sets variables to be whats located in the saveData
 	saveDataValues.miningValues.set("selectedDuck", $"Duck Selection/DuckList".get_item_text(index).remove_chars("1234567890.- "))
 	miningValue = 2 * saveDataValues.Duckies.get(saveDataValues.miningValues.get("selectedDuck")).get("strength") * (saveDataValues.Duckies.get(saveDataValues.miningValues.get("selectedDuck")).get("timesMined") / 5 + 1)
 	saveDataValues.miningValues.set("MiningValue", miningValue)
 	
 func _on_continue_pressed() -> void:
+	#Sets the visuals and sets the current tab to waiting tab
 	if saveDataValues.miningValues.get("selectedDuck") != "":
 		current_tab = current_tab + 1
 		saveDataValues.miningValues.set("duckMiningStartTime", saveDataValues.igt)
@@ -117,6 +120,7 @@ func _on_continue_pressed() -> void:
 		saveDataValues.Duckies.get(saveDataValues.miningValues.get("selectedDuck")).set("timesMined", saveDataValues.Duckies.get(saveDataValues.miningValues.get("selectedDuck")).get("timesMined") + 1)
 		self.get_parent().find_child("ErrorText").visible = false
 	else:
+		#Error just in case there is no duck selected
 		self.get_parent().find_child("ErrorText").visible = true
 		self.get_parent().find_child("ErrorText").text = "Error: No Duck Selected"
 
@@ -157,6 +161,7 @@ func _on_option_3_pressed() -> void:
 		miniGameOptionPicked(rightValue)
 
 func obtainMinerals():
+	#Loops through the mineral list and solves for how much of the mineral the duck gets based a randomly generated float and the mineral's odds
 	current_tab = 2
 	obtainedMinerals = {}
 	for x in MineralList:
@@ -168,6 +173,7 @@ func obtainMinerals():
 		$ObtainMinerals/ItemList.add_item(str(obtainedMinerals.get(x)) + " " + x)
 
 func duckGotInjured():
+	#sets the hospital values if the duck gets injured
 	var duck = saveDataValues.miningValues.get("selectedDuck")
 	saveDataValues.hospitalValues.set(duck, {
 		"timeTillLeave" : 120,
@@ -188,6 +194,7 @@ func duckGotInjured():
 	$ReturnTimer/MiniGameSoundEffects.play()
 
 func miniGameOptionPicked(pathChosen: int):
+	#Based on what path was chosen, will get that value and sets up what happens if you pick the positive option, negative option, and middle option
 	var v = randf()
 	if pathChosen == 0:
 		saveDataValues.miningValues.set("duckMiningStartTime", saveDataValues.miningValues.get("duckMiningStartTime") + 30 * v)
@@ -204,6 +211,7 @@ func miniGameOptionPicked(pathChosen: int):
 	
 	if pathChosen == -1:
 		duckGotInjured()
-	
+
+	#Errors
 	if pathChosen != -1 && pathChosen != 0 && pathChosen != 1:
 		printerr("Minigame error: Value Incompatible")
